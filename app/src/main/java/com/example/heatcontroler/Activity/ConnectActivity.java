38d13f701c1806c8e736fa295e63bc2c31e2dd11
regaliastar <1183080130@qq.com>
2018-06-01 17:46:56 +0800

    httpUtils

--- app/src/main/java/com/example/heatcontroler/Activity/ConnectActivity.java
+++ app/src/main/java/com/example/heatcontroler/Activity/ConnectActivity.java
@@ -3,16 +3,20 @@ package com.example.heatcontroler.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.heatcontroler.R;
+import com.example.heatcontroler.utils.HttpUtil;
 
 public class ConnectActivity extends AppCompatActivity {
     EditText productId;
     EditText apiKey;
     Button connectBtn;
+    Button getTempBtn;
     final String PID = "130185";
     final String APIKEY = "L63i2WDONLZOth1sE=FNXckQG20=";
 
@@ -26,17 +30,43 @@ public class ConnectActivity extends AppCompatActivity {
         apiKey = findViewById(R.id.apiKey);
         apiKey.setText(APIKEY);
         connectBtn = findViewById(R.id.connectBtn);
-        connectBtn.setOnClickListener(new View.OnClickListener() {
+        getTempBtn = findViewById(R.id.getTemp);
+        getTempBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                HttpUtil httpUtil = new HttpUtil();
+                String getContainer = httpUtil.doGet("http://www.baidu.com");   //http://api.heclouds.com/keys
+                Log.d("getContainer"," "+getContainer);
+            }
+        });
+    }
+/*
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.connectBtn:
+                Toast.makeText(this, "connectBtn", Toast.LENGTH_SHORT).show();
                 Connecting();
 
 //                Intent intent = new Intent(ConnectActivity.this, StateActivity.class);
 //                startActivity(intent);
-            }
-        });
+                break;
+            case R.id.getTemp:
+                Toast.makeText(this, "getTemp", Toast.LENGTH_SHORT).show();
+                HttpUtil httpUtil = new HttpUtil();
+                String getContainer = httpUtil.doGet("http://api.heclouds.com/keys");
+                Log.d("getContainer",getContainer);
+                break;
+            case R.id.setTemp:
+                Toast.makeText(this, "setTemp", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                Log.d("onClick","default");
+                break;
+        }
     }
-
+*/
     private void Connecting(){
 
     }
